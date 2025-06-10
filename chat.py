b99a7f00f9ab260c7b8799110017e3bef77ca89e
73962869 <taeyeol2483@gmail.com>
2025-06-10 13:52:40 +0900

    refactor: modularize LLM, vector store, and QA chain setup in llm.py

--- chat.py
+++ chat.py
@@ -10,33 +10,28 @@ if 'message_list' not in st.session_state:
     st.session_state.message_list = []
 
 
-print(f'before: {st.session_state.message_list}')
-
-
 ## 이전 채팅 내용 화면 출력
 for message in st.session_state.message_list:
     with st.chat_message(message['role']):
         st.write(message['content'])
 
 
-## prompt 창(채팅 창) #############################################################
+## 채팅 메세지
 placeholder = '전세사기피해와 관련된 궁금한 내용을 질문하세요.'
-if user_question := st.chat_input(placeholder=placeholder):
+if user_question := st.chat_input(placeholder=placeholder): ## prompt 창
+    ## 사용자 메세지
     with st.chat_message('user'):
         ## 사용자 메시지 화면 출력
         st.write(user_question)
     st.session_state.message_list.append({'role': 'user', 'content': user_question})
 
-    ai_message = get_ai_message(user_question)
+    ## AI 메세지
+    with st.spinner('답변을 생성하는 중입니다.'):
+        ai_message = get_ai_message(user_question)
 
-
-    with st.chat_message('ai'):
-        ## AI 메세지 화면 출력
-        st.write(ai_message)
-    st.session_state.message_list.append({'role': 'ai', 'content': ai_message})
-
+        with st.chat_message('ai'):
+            ## AI 메세지 화면 출력
+            st.write(ai_message)
+        st.session_state.message_list.append({'role': 'ai', 'content': ai_message})
 
 print(f'after: {st.session_state.message_list}')
-
-
-print(user_question)
